79d22e6afc3c59eb15bf80db766507ea843a4155
Farsit-007 <robayatfarsit@gmail.com>
2025-09-20 01:39:45 +0600

    Updated

--- src/auth/decorators/active-user.decorator.ts
+++ src/auth/decorators/active-user.decorator.ts
@@ -2,9 +2,10 @@
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { ActiveUserInterface } from '../interfaces/active-user.interface';
 
 export const ActiveUser = createParamDecorator(
-  (field: unknown, ctx: ExecutionContext) => {
+  (field: keyof ActiveUserInterface | undefined, ctx: ExecutionContext) => {
     const request = ctx.switchToHttp().getRequest();
     return request.user;
   },
