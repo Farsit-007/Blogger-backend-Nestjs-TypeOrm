9bdc217fb2b200829594b7c420d7f57498e31aa4
Farsit-007 <robayatfarsit@gmail.com>
2025-09-20 02:00:34 +0600

    Updated

--- src/posts/providers/create-post.provider.ts
+++ src/posts/providers/create-post.provider.ts
@@ -1,10 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  ConflictException,
+  Injectable,
+} from '@nestjs/common';
 import { CreatePostDto } from '../dtos/create-post.dto';
 import { UsersService } from 'src/users/providers/users.service';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { TagsService } from 'src/tags/providers/tags.service';
 import { Post } from '../post.entity';
+import { ActiveUserInterface } from 'src/auth/interfaces/active-user.interface';
+import { User } from 'src/users/user.entity';
+import { Tag } from 'src/tags/tags.entity';
 
 @Injectable()
 export class CreatePostProvider {
@@ -17,12 +24,20 @@ export class CreatePostProvider {
     private readonly tagService: TagsService,
   ) {}
 
-  public async create(createPostDto: CreatePostDto) {
-    // Find author from dayabase based on authorId
-    const author = await this.usersService.findOneById(createPostDto.authorId);
+  public async create(createPostDto: CreatePostDto, user: ActiveUserInterface) {
+    let author: User | null = null;
+    let tags: Tag[] | [] = [];
+    try {
+      // Find author from dayabase based on authorId
+      author = await this.usersService.findOneById(user.sub);
 
-    const tags = await this.tagService.findmultiplesTags(createPostDto.tags!);
-
+      tags = await this.tagService.findmultiplesTags(createPostDto.tags!);
+    } catch (error) {
+      throw new ConflictException(error);
+    }
+    if (createPostDto.tags?.length !== tags?.length) {
+      throw new BadRequestException('Please check your tags');
+    }
     // Create Post
     const post = this.postsRepository.create({
       ...createPostDto,
