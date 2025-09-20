4105ccbd9cce7eaf61a0954af7b178faa6d587e7
Farsit-007 <robayatfarsit@gmail.com>
2025-09-20 14:27:15 +0600

    Updated

--- src/auth/providers/auth.service.ts
+++ src/auth/providers/auth.service.ts
@@ -2,6 +2,8 @@ import { forwardRef, Inject, Injectable } from '@nestjs/common';
 import { UsersService } from 'src/users/providers/users.service';
 import { SigninDto } from '../dto/signin.dto';
 import { SignInProvider } from './sign-in.provider';
+import { RefreshTokenDto } from '../dto/refresh-token.dto';
+import { RefreshTokenProvider } from './refresh-token.provider';
 
 @Injectable()
 export class AuthService {
@@ -9,12 +11,13 @@ export class AuthService {
     @Inject(forwardRef(() => UsersService))
     private readonly usersService: UsersService,
     private readonly signInProvider: SignInProvider,
+    private readonly refreshTokenProvider: RefreshTokenProvider,
   ) {}
   public async signIn(signInDto: SigninDto) {
     return await this.signInProvider.signIn(signInDto);
   }
 
-  public isAuth() {
-    return true;
+  public async refreshTokens(refreshTokenDto: RefreshTokenDto) {
+    return await this.refreshTokenProvider.refreshTokens(refreshTokenDto);
   }
 }
