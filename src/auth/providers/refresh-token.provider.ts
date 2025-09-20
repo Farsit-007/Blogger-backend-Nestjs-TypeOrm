776f15de535e52b24b659a6502029b1b972b8dfc
Farsit-007 <robayatfarsit@gmail.com>
2025-09-20 14:19:37 +0600

    Updated

--- src/auth/providers/refresh-token.provider.ts
+++ src/auth/providers/refresh-token.provider.ts
@@ -1,10 +1,16 @@
-import { forwardRef, Inject, Injectable } from '@nestjs/common';
+import {
+  forwardRef,
+  Inject,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { RefreshTokenDto } from '../dto/refresh-token.dto';
 import { JwtService } from '@nestjs/jwt';
 import jwtConfig from '../config/jwt.config';
 import type { ConfigType } from '@nestjs/config';
 import { GenerateTokensProvider } from './generate-tokens.provider';
 import { UsersService } from 'src/users/providers/users.service';
+import { ActiveUserInterface } from '../interfaces/active-user.interface';
 
 @Injectable()
 export class RefreshTokenProvider {
@@ -18,8 +24,22 @@ export class RefreshTokenProvider {
   ) {}
 
   public async refreshTokens(refreshTokenDto: RefreshTokenDto) {
-    // Verify the refresh token using jwtService
-    // Fetch user from the database
-    // Generate the tokens
+    try {
+      // Verify the refresh token using jwtService
+      const { sub } = await this.jwtService.verifyAsync<ActiveUserInterface>(
+        refreshTokenDto.refreshToken,
+        {
+          secret: this.jwtConfiguration.secret,
+          audience: this.jwtConfiguration.audience,
+          issuer: this.jwtConfiguration.issuer,
+        },
+      );
+      // Fetch user from the database
+      const user = await this.usersService.findOneById(sub);
+      // Generate the tokens
+      return await this.generateTokensProviders.generateTokens(user);
+    } catch (error) {
+      throw new UnauthorizedException(error);
+    }
   }
 }
