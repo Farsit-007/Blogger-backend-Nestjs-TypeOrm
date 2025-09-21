a22841aa02f7abd4179b6d8042e34973a9adf50c
Farsit-007 <robayatfarsit@gmail.com>
2025-09-22 02:02:26 +0600

    Updated

--- src/users/providers/create-google-user.provider.ts
+++ src/users/providers/create-google-user.provider.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from '../user.entity';
+import { GoogleUserInterface } from '../interfaces/google-user-interface';
 
 @Injectable()
 export class CreateGoogleUserProvider {
@@ -10,5 +11,14 @@ export class CreateGoogleUserProvider {
     private readonly usersRepository: Repository<User>,
   ) {}
 
-  public async createGooglUser(googleUser){}
+  public async createGooglUser(googleUser: GoogleUserInterface) {
+    try {
+      const user = this.usersRepository.create(googleUser);
+      return await this.usersRepository.save(user);
+    } catch (error) {
+      throw new ConflictException(error, {
+        description: 'Cloud not create a user',
+      });
+    }
+  }
 }
