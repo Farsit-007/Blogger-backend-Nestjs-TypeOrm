6037454e8d79a3e0a79ee683a2f3daeff18efed3
Farsit-007 <robayatfarsit@gmail.com>
2025-09-22 02:00:42 +0600

    Updated

--- src/users/providers/create-google-user.provider.ts
+++ src/users/providers/create-google-user.provider.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from '../user.entity';
 
 @Injectable()
-export class CreateGoogleUserProvider {}
+export class CreateGoogleUserProvider {
+  constructor(
+    @InjectRepository(User)
+    private readonly usersRepository: Repository<User>,
+  ) {}
+
+  public async createGooglUser(googleUser){}
+}
