833195af1a94367e3e3f2030ddebe4dde331ff33
Farsit-007 <robayatfarsit@gmail.com>
2025-09-23 00:37:07 +0600

    Updated

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { DataResponseInterceptor } from './common/interceptors/data-response/data-response.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -28,6 +29,7 @@ async function bootstrap() {
 
   //Enable Cors
   app.enableCors();
+  app.useGlobalInterceptors(new DataResponseInterceptor());
   await app.listen(process.env.PORT ?? 5000);
 }
 bootstrap();
